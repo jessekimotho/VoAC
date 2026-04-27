879c476d92f09147b9e3f8eb85c0c245c3fe5afa
Jesse Kimotho <jnjug.97@gmail.com>
2026-04-27 16:29:49 +0300

    Remove unused import to test if it causes error

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,31 +1,9 @@
-import { listAllPublishedPosts } from '$lib/server/posts';
-
 export async function load() {
-	// Temporarily return empty data to test if Supabase is the issue
+	// Temporarily return empty data to test if imports are the issue
 	return {
 		featured: null,
 		recent: [],
 		allPosts: []
 	};
-
-	// Original code:
-	// try {
-	// 	const posts = await listAllPublishedPosts();
-	// 	const [featured, ...recent] = posts;
-
-	// 	return {
-	// 		featured,
-	// 		recent,
-	// 		allPosts: posts
-	// 	};
-	// } catch (error) {
-	// 	console.error('Error loading homepage:', error);
-	// 	// Return empty arrays to prevent 500 error
-	// 	return {
-	// 		featured: null,
-	// 		recent: [],
-	// 		allPosts: []
-	// 	};
-	// }
 }
 
